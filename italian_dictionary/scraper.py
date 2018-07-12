8342514caa10b4415a1f5e7576e304591913881e
Sphoneix <simone.pugliese21@gmail.com>
2018-07-12 14:35:36 +0200

    add Url value in get_alldata

--- italian_dictionary/scraper.py
+++ italian_dictionary/scraper.py
@@ -56,6 +56,7 @@ def get_defs(word):
 
 def get_all_data(word):
     data = {'lemma': None, 'pronuncia': None,'grammatica': None,  'definizione': None, 'locuzioni': None,}
+    data['url'] = URL.format(word)
     data['definizione'] = get_defs(word)
     data['lemma'] = get_lemma(word)
     data['pronuncia'] = get_pronuncia(word)
