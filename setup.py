6758bf1afd6bfd85f874936f8f878246ab8209b4
Simone Pugliese <simone.pugliese21@gmail.com>
2020-04-23 13:52:56 +0200

    Updating version 1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="italian_dictionary",
-    version="1.4",
+    version="1.5",
     author="sphoneix",
     author_email="simone.pugliese21@gmail.com",
     description="A package which retrieves meaning and other informations about italian words.",
@@ -13,11 +13,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/sphoneix22/italian_dictionary",
     packages=setuptools.find_packages(),
+    python_requires='>=3.6',
     install_requires = [
         'beautifulsoup4'
     ],
     classifiers=(
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
